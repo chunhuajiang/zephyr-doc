125eb78530d06c47bd0363fd2bf6da066484117f
Kitty(chun hua) Jiang <Kitty_Jiang@sigmadesigns.com>
2016-11-30 18:45:13 +0800

    修改github显示方式

--- doc/conf.py
+++ doc/conf.py
@@ -305,4 +305,11 @@ breathe_default_project = "Zephyr"
 
 html_context = {
     'show_license': html_show_license,
+    # Enable the "Edit in GitHub link within the header of each page.
+    'display_github': True,
+    # Set the following variables to generate the resulting github URL for each page. 
+    # Format Template: https://{{ github_host|default("github.com") }}/{{ github_user }}/{{ github_repo }}/blob/{{ github_version }}{{ conf_py_path }}{{ pagename }}{{ suffix }}
+    'github_user': 'tidyjiang8',
+    'github_repo': 'zephyr-doc',
+    'github_version': 'v1.6.0/doc/' 
 }
